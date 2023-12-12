47a2b9e05551459722b0931cff25555a1c47f442
zxuanhong <z7630853@163.com>
2023-12-12 22:54:56 +0800

    init

--- src/main/java/com/example/demo/UserTaskJobHandler.java
+++ src/main/java/com/example/demo/UserTaskJobHandler.java
@@ -1,16 +1,22 @@
 package com.example.demo;
 
+import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.client.api.worker.JobHandler;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Component
 @Slf4j
 public class UserTaskJobHandler implements JobHandler {
+
+    @Autowired
+    private ZeebeClient zeebeClient;
+
 
     @Override
     @JobWorker(timeout = 2592000000L, autoComplete = false)
